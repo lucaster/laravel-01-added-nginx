722e71dc82ceb3665ad196aa1c5a79dbc21ce359
lucaster <luca.cavagnoli@gmail.com>
2021-02-04 18:49:28 +0100

    added dependencies between services

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 docker-compose build
 
-# use composer via container to scaffold the laravel project:
+# use composer via the utility composer container to scaffold the laravel project:
 # the . will correspond to WORKDIR /var/www/html
 docker-compose run --rm composer create-project --prefer-dist laravel/laravel .
 
 # then remember to update mysql coordinates in scr/.env
 
 # force docker-compose to reevaluate dockerfiles and rebuild images
-docker-compose up -d --build server php mysql
+docker-compose up -d --build server
